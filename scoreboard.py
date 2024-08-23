d9703788eda92be5407355df380acda30b589921
niti21 <nithishk12@gmail.com>
2024-08-24 01:42:50 +0530

    change to record high score

--- scoreboard.py
+++ scoreboard.py
@@ -8,19 +8,33 @@ class Scoreboard(Turtle):
     def __init__(self,) -> None:
         super().__init__()
         self.score = 0
+        self.high_score = self.get_prev_high_score()
         self.sety(265)
         self.hideturtle()
         self.color("white")
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALLIGNMENT, font=FONT)
+        self.clear()
+        self.write(
+            f"Score: {self.score} High Score: {self.high_score}", align=ALLIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.sety(0)
-        self.write(f"Game Over", align=ALLIGNMENT, font=FONT)
+    def reset_game(self):
+        if self.score >= self.high_score:
+            self.high_score = self.score
+            self.write_new_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def get_prev_high_score(self):
+        with open('scorecard.txt', 'r') as f:
+            prev_high_score = int(f.read())
+        return prev_high_score
+
+    def write_new_high_score(self):
+        with open('scorecard.txt', 'w') as f:
+            f.write(str(self.high_score))
